5ca509ad1cee357caaaf031e81ccc42d8e9918bd
Abel Borit <abelboritguitton@gmail.com>
2024-04-05 10:21:44 -0500

    [FEAT] create Counter2.ts

---
+++ src/components/Counter2.ts
@@ -0,0 +1,9 @@
+export function setupCounter2(element: HTMLButtonElement) {
+  let counter = 0;
+  const setCounter = (count: number) => {
+    counter = count;
+    element.innerHTML = `count is ${counter}`;
+  };
+  element.addEventListener("click", () => setCounter(counter + 2));
+  setCounter(0);
+}
